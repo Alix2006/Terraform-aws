ee675b03711e5165435e3899437c3cb2e1697f73
AA <asgari2583@yahoo.com>
2021-11-12 18:15:46 -0800

    first commit

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "region" {
+        default = "us-west-2"
+}
+
+variable "region" {
+        default = "us-west-2"
+}
+
+variable "profile" {
+    description = "AWS credentials profile you want to use"
+}
